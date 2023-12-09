d04a7d7e2ac5d3ea81a9021c156c25041bb5f5bd
rkrakhikumari <rkrakhikumari012@gmail.com>
2023-12-09 11:48:09 +0530

    turtle roadcrossing game

---
+++ main.py
@@ -0,0 +1,41 @@
+from turtle import Turtle, Screen
+import time
+from player import *
+from car_manager import *
+from scoreboard import *
+
+screen = Screen()
+screen.setup(width=600, height= 600)
+screen.tracer(0)
+screen.bgcolor("grey")
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(player.go_up,"Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) <20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+
+
+screen.exitonclick()
